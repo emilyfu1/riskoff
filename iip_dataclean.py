0484407212b048a0cb45967a38e5cad9c890c471
Emily Fu <83727721+emilyfu1@users.noreply.github.com>
2023-12-01 12:31:01 -0500

    fix heatmap code

--- iip_dataclean.py
+++ iip_dataclean.py
@@ -3,11 +3,17 @@ import numpy as np
 from dotenv import dotenv_values, find_dotenv
 import os
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.colors as colors
+from matplotlib.colors import LinearSegmentedColormap
+
 # this looks for your configuration file and then reads it as a dictionary
 config = dotenv_values(find_dotenv())
 
 # set path using the dictionary key for which one you want
 path_rawdata = os.path.abspath(config["RAWDATA"]) + '\\'
+path_figures = os.path.abspath(config["FIGURES"]) + '\\'
 
 def iip_dataclean(data):
     # country codes
@@ -45,3 +51,114 @@ def iip_dataclean(data):
     iip_long['value'] = pd.to_numeric(iip_long['value'])
     
     return iip_long
+
+# my plan for dealing with dates:
+# year values: December of that year xxxx-12-31
+# mid-year values: June of that year xxxx-06-30
+def convert_date(row):
+    year = int(row['date'][:4])
+    
+    if 'S' not in row['date']:
+        return f'{year}-12-31'
+    else:
+        return f'{year}-06-30'
+
+def filter_data_heatmap(data, indicator, agg=None):
+    if 'A' in indicator:
+        indicatorname = 'Assets'
+    else:
+        indicatorname = 'Liabilities'
+
+    currency = indicator[-3:]
+    # countries that i want
+    # just showing as good a picture as possible for data availability
+    countries = ['USA', 'CAN', 'KOR', 'GBR', 'JPN', 'EUR', 'IRL', 'CHE', 'AUS', 'NZL', 'SWE', 
+                'NOR', 'FIN', 'DNK', 'CZE', 'ZAF', 'DEU', 'AUT', 'BEL', 'BOL', 'BRA', 'CHL', 
+                'CHN', 'COL', 'EST', 'FRA', 'IDN', 'IND', 'ISL', 'ISR', 'ITA', 'MEX', 'MYS', 
+                'NLD', 'POL', 'QAT', 'ROU', 'RUS', 'SAU', 'SVK', 'THA', 'TUR', 'UKR', 'HKG',
+                'UGA', 'VNM', 'TWN', 'RWA', 'QAT', 'PRT', 'SYR', 'PHL', 'PAK', 'DOM', 'ARG']
+    
+    forheatmap = data[data['Indicator Code'] == indicator][['iso3', 'date', 'value']]
+
+    # insert countries based on my list by creating a dataframe to merge
+    df_countries = pd.DataFrame(countries, columns=['iso3'])
+    # add date range to country list
+    df_countries['date'] = df_countries['iso3'].apply(lambda x: [f'{year}' for year in list(set(forheatmap['date']))])
+    # explode list of dates
+    df_countries = df_countries.explode('date', ignore_index=True)
+    if not (indicator == 'I_L_T_T_T_BP6_USD' or indicator == 'I_A_T_T_T_BP6_USD'):
+        df_countries['date'] = pd.to_datetime(df_countries['date'])
+    # merge with existing data
+    forheatmap = pd.merge(left=forheatmap, right=df_countries, how='outer', on=['date', 'iso3'])
+
+    if agg == 'firstcountry':
+        version = 'first country'
+    else:
+        agg = 'counterpart country'
+    if indicator == 'I_L_T_T_T_BP6_USD' or indicator == 'I_A_T_T_T_BP6_USD':
+        # fix dates
+        # insert missing years by copying the 1997 data and replacing everyting with nans and then merging
+        # honestly kinda shitty way to do this
+        data_1998 = forheatmap[forheatmap['date'] == '1997'] 
+        data_1998.loc[data_1998['date'] == '1997', 'date'] = '1998'
+        data_1998.loc[data_1998['value'].notnull(), 'value'] = np.nan
+        data_1999 = data_1998.copy()
+        data_1999.loc[data_1999['date'] == '1998', 'date'] = '1999'
+        data_2000 = data_1999.copy()
+        data_2000.loc[data_2000['date'] == '1999', 'date'] = '2000'
+        forheatmap = pd.concat([forheatmap, data_1998, data_1999, data_2000])
+
+        # convert format
+        forheatmap['date'] = forheatmap.apply(convert_date, axis=1)
+        # insert missing half-year values
+        for index, row in forheatmap.iterrows():
+                year = int(row['date'][:4])
+                middle_of_year_date = f'{year}-06-30'
+
+                # add rows if the date doesn't appear in the data
+                if not any(forheatmap['date'] == middle_of_year_date):
+                    # Create a new row with the middle-of-year date
+                    new_row = row.copy()
+                    new_row['date'] = middle_of_year_date
+                    new_row['value'] = np.nan
+                    forheatmap.loc[len(forheatmap)] = new_row
+        
+        forheatmap['date'] = pd.to_datetime(forheatmap['date'])
+        forheatmap = forheatmap.sort_values('date').reset_index(drop=True)
+
+    # filter by country
+    forheatmap = forheatmap[forheatmap['iso3'].isin(countries)]
+
+    # Create a pivot table to reshape the data for the heatmap
+    heatmap_data = forheatmap.pivot_table(index='iso3', columns='date', values='value', aggfunc='count', fill_value=0)
+
+    # Create a binary matrix indicating the presence of data
+    binary_matrix = heatmap_data > 0
+
+    # Reorder the rows based on the number of True values in each row (descending order)
+    # binary_matrix = binary_matrix.loc[binary_matrix.sum(axis=1).sort_values(ascending=False).index]
+
+    # Heatmap
+    colours = (colors.hex2color('#CED4DA'),colors.hex2color('#339AF0'))
+    cmap = LinearSegmentedColormap.from_list('Custom', colours, len(colours))
+    xint = [i.year for i in binary_matrix.columns.tolist()]
+    sns.set(font_scale = 1.1)
+    plt.subplots(figsize=(20,15))
+    ax = sns.heatmap(binary_matrix, cmap=cmap, yticklabels=True, xticklabels=xint  ,linewidths=0.01, cbar=False)
+    ax.set(xlabel="", ylabel="")
+    ax.set_xticklabels(ax.get_xticklabels(),rotation = 0)
+    ax.set_xticks(ax.get_xticks()[::8])
+    if indicator == 'I_L_T_T_T_BP6_USD' or indicator == 'I_A_T_T_T_BP6_USD':
+        plt.title('Availability of CPIS data (' + indicator + ': ' + indicatorname + currency + ') aggregated by ' + version + ', select countries')
+    else:
+        plt.title('Availability of IIP data (' + indicator + ': ' + indicatorname + ') when denominated in ' + currency + ', select countries')
+    plt.show()
+
+    # Export graph as pdf
+    fig = ax.get_figure()
+    if indicator == 'I_L_T_T_T_BP6_USD' or indicator == 'I_A_T_T_T_BP6_USD':
+        fig.savefig(path_figures + 'availability_' + indicator + '_' + agg + '.pdf', bbox_inches = 'tight')
+    else:
+        fig.savefig(path_figures + 'availability_' + indicator + '.pdf', bbox_inches = 'tight')
+
+    return forheatmap
